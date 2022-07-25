6674a58c458d15bafc27834595780e345b275d79
J0SHMEYER <100731166+J0SHMEYER@users.noreply.github.com>
2022-07-25 16:37:09 +1200

    basic input box

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Declare a new state variable, which we'll call "pokemonName"
+  const [spacecraftName, setSpacecraft] = useState("");
+
   return (
     <div>
-        <p>
-            Hello, world
-        </p>
+      <h1>
+        Spacecraft Search
+      </h1>
+      
+      <div>
+        <label>Spacecraft Name</label><br/>
+        <input type="text" id="spacecraft-name" name="spacecraft-name" onChange={e => setSpacecraft(e.target.value)}/><br/>
+        <button onClick={search}>
+        Search
+        </button>
+      </div>
+
+      <p>
+        You have entered {spacecraftName}
+      </p>
     </div>
   );
+
+  function search(){
+      alert("Search button has been clicked!");
+  }
 }
 
 export default App;
