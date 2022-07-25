0cbb0dc03f26d7c9a46e47920c72f6c066478464
J0SHMEYER <100731166+J0SHMEYER@users.noreply.github.com>
2022-07-25 18:35:33 +1200

    call API and search for image

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,49 @@
+import axios from "axios";
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   // Declare a new state variable, which we'll call "pokemonName"
   const [spacecraftName, setSpacecraft] = useState("");
+  const [spacecraftInfo, setSpacecraftInfo] = useState<undefined | any>(undefined);
 
+  const SPACECRAFT_BASE_URL = "https://lldev.thespacedevs.com/2.2.0";
   return (
     <div>
-      <h1>
-        Spacecraft Search
-      </h1>
+      <h1>Spacecraft Search</h1>
       
       <div>
-        <label>Spacecraft Name</label><br/>
-        <input type="text" id="spacecraft-name" name="spacecraft-name" onChange={e => setSpacecraft(e.target.value)}/><br/>
-        <button onClick={search}>
-        Search
-        </button>
+        <label>Spacecraft Name</label>
+        <br/>
+        <input 
+          type="text" 
+          id="spacecraft-name" 
+          name="spacecraft-name" 
+          onChange={e => setSpacecraft(e.target.value)}
+        />
+        <br/>
+        <button onClick={search}>Search</button>
       </div>
 
-      <p>
-        You have entered {spacecraftName}
-      </p>
+      <p>You have entered {spacecraftName}</p>
+
+      {spacecraftInfo === undefined ? (
+        <p>Spacecraft not found</p>
+      ) : (
+      <div id ="spacecraft-result">
+        <img src={spacecraftInfo.image_url} />
+      </div>
+      )};
     </div>
   );
 
   function search(){
-      alert("Search button has been clicked!");
+    axios.get(SPACECRAFT_BASE_URL + "/config/spacecraft/?search=" + spacecraftName).then((res) => {
+      const newURL = res.data.results[0].url;
+      axios.get(newURL).then((resNew) => {
+        setSpacecraftInfo(resNew.data);
+      });
+    });
   }
 }
 
