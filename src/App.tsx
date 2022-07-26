47cd1df97cee96bf2dc0ff1b03bef2bf87daf899
J0SHMEYER <100731166+J0SHMEYER@users.noreply.github.com>
2022-07-26 19:21:06 +1200

    centered web components

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,20 @@ import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 
 function App() {
-  // Declare a new state variable, which we'll call "pokemonName"
+  // Declare new state variable called "spacecraftName"
   const [spacecraftName, setSpacecraftName] = useState("");
   const [spacecraftInfo, setSpacecraftInfo] = useState<undefined | any>(undefined);
 
   const SPACECRAFT_BASE_URL = "https://lldev.thespacedevs.com/2.2.0";
   return (
     <div>
+
       <div className="search-field">
-        <h1>Spacecraft Search</h1>
-      
+        <h1 style={{ display: "flex", justifyContent: "center" }}>
+          Spacecraft Search
+        </h1>    
+
+
         <div style={{ display: "flex", justifyContent: "center" }}>
           <TextField
             id="search-bar"
@@ -40,16 +44,24 @@ function App() {
         </div>
       </div>
 
+
       {spacecraftInfo === undefined ? (
         <div></div>
       ) : (
-      <div id ="spacecraft-result">
-        <p>Result {spacecraftInfo.name}</p>
-        <img src={spacecraftInfo.image_url} />
-        <p>Name of agency {spacecraftInfo.agency.name}</p>
-        <p>{spacecraftInfo.agency.description}</p>
+      <div id ="spacecraft-result" style={{maxWidth: "500px", margin: "0 auto", padding: "30px 100px 0px 100px"}}>
+        <div style={{margin: "auto", textAlign: "center"}}>
+          <p>Result: {spacecraftInfo.name}</p>
+        </div>
+        <div style={{textAlign: "center" }}>
+          <img src={spacecraftInfo.image_url} 
+          style={{ height: "400px" ,width:"465px", objectFit: "cover", }}/>
+        </div>
+        <p style={{margin: "0 auto", textAlign: "center", padding: "30px 50px 0px 50px"}}>Name of agency {spacecraftInfo.agency.name}</p>
+        <p style={{margin: "0 auto", textAlign: "center", padding: "30px 10px 0px 10px"}}>{spacecraftInfo.agency.description}</p>
       </div>
       )};
+
+
     </div>
   );
 
