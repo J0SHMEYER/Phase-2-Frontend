dc9192536e70c69e53083c4634f727b42dcf5e50
J0SHMEYER <100731166+J0SHMEYER@users.noreply.github.com>
2022-07-26 15:05:28 +1200

    added MUI search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,53 @@
 import axios from "axios";
 import { useState } from 'react';
 import './App.css';
+import { Box, Button, Grid, Paper, Skeleton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 
 function App() {
   // Declare a new state variable, which we'll call "pokemonName"
-  const [spacecraftName, setSpacecraft] = useState("");
+  const [spacecraftName, setSpacecraftName] = useState("");
   const [spacecraftInfo, setSpacecraftInfo] = useState<undefined | any>(undefined);
 
   const SPACECRAFT_BASE_URL = "https://lldev.thespacedevs.com/2.2.0";
   return (
     <div>
-      <h1>Spacecraft Search</h1>
+      <div className="search-field">
+        <h1>Spacecraft Search</h1>
       
-      <div>
-        <label>Spacecraft Name</label>
-        <br/>
-        <input 
-          type="text" 
-          id="spacecraft-name" 
-          name="spacecraft-name" 
-          onChange={e => setSpacecraft(e.target.value)}
-        />
-        <br/>
-        <button onClick={search}>Search</button>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <TextField
+            id="search-bar"
+            className="text"
+            value={spacecraftName}
+            onChange={(prop) => {
+              setSpacecraftName(prop.target.value);
+            }}
+            label="Enter a Spacecraft..."
+            variant="outlined"
+            placeholder="Search..."
+            size="small"
+          />
+          <Button
+            onClick={() => {
+              search();
+            }}
+          >
+            <SearchIcon style={{ fill: "blue" }} />
+            Search
+          </Button>
+        </div>
       </div>
 
-      <p>You have entered {spacecraftName}</p>
-
       {spacecraftInfo === undefined ? (
-        <p>Spacecraft not found</p>
+        <div></div>
       ) : (
       <div id ="spacecraft-result">
+        <p>Result {spacecraftInfo.name}</p>
         <img src={spacecraftInfo.image_url} />
+        <p>Name of agency {spacecraftInfo.agency.name}</p>
+        <p>{spacecraftInfo.agency.description}</p>
       </div>
       )};
     </div>
